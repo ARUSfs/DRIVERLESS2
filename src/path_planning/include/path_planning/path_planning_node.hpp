80f58b0e78f7bb24bd9ffd2bbcadec4071e79113
Ignacio Sánchez <igsais12@gmail.com>
2024-10-04 13:59:36 +0200

    Create base strucure for Path Planning node

---
+++ src/path_planning/include/path_planning/path_planning_node.hpp
@@ -0,0 +1,9 @@
+#include <CGAL/Triangle_2.h>
+#include <rclcpp/rclcpp.hpp>
+
+class PathPlanningNode : public rclcpp::Node
+{
+    public:
+        PathPlanningNode();
+
+};
---
+++ src/path_planning/src/path_planning_node.cpp
@@ -0,0 +1,6 @@
+#include "path_planning/path_planning_node.hpp"
+#include <CGAL/Delaunay_triangulation_2.h>
+
+int main(){
+    return 0;
+}
