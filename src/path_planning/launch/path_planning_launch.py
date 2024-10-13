c4d4d29789609c3d15195b8f45e23af9d4552858
Ignacio Sánchez <igsais12@gmail.com>
2024-10-13 18:06:38 +0200

    feat: configure launch file for path planning node

--- src/path_planning/launch/path_planning_launch.py
+++ src/path_planning/launch/path_planning_launch.py
@@ -7,4 +7,16 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    pass
+    package_name = "path_planning"
+    package_share_directory = get_package_share_directory(package_name)
+    config_file = os.path.join(package_share_directory, "config", "path_planning_config.yaml")
+
+    return LaunchDescription([
+        Node(
+            package="path_planning",
+            executable="path_planning_exec",
+            name="path_planning",
+            output="screen",
+            parameters=[config_file]
+        )
+    ])
