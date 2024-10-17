6b22801b24cdc081563a29286f38b57f5bc98104
Ignacio Sánchez <igsais12@gmail.com>
2024-10-17 16:23:35 +0200

    fix: Solves issue #12, rename class PathPlanning to match the rest of the nodes

--- src/path_planning/include/path_planning/path_planning_node.hpp
+++ src/path_planning/include/path_planning/path_planning_node.hpp
@@ -1,15 +1,22 @@
-#include <CGAL/Triangle_2.h>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
+#include <path_planning/delaunay_triangulation.hpp>
+#include <common_msgs/msg/point2d.hpp>
+#include <common_msgs/msg/triangle.hpp>
+#include <common_msgs/msg/triangulation.hpp>
+#include "path_planning/PointXYZColorScore.h"
 
-class PathPlanningNode : public rclcpp::Node
+
+class PathPlanning : public rclcpp::Node
 {
     public:
-        PathPlanningNode();
+        PathPlanning();
     private:
         std::string kPerceptionTopic;
-
+        std::string kTriangulationTopic;
         rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr perception_sub_;
-
-        void perception_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+        rclcpp::Publisher<common_msgs::msg::Triangulation>::SharedPtr triangulation_pub_;
+        void perception_callback(sensor_msgs::msg::PointCloud2::SharedPtr msg);
+        DelaunayTriangulation triangulation_class_;
+        GEOM_FADE2D::Fade_2D triangulation_geom_;
 };
--- src/path_planning/src/path_planning_node.cpp
+++ src/path_planning/src/path_planning_node.cpp
@@ -1,28 +1,36 @@
 #include "path_planning/path_planning_node.hpp"
+#include <path_planning/delaunay_triangulation.hpp>
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 
-PathPlanningNode::PathPlanningNode() : Node("path_planning")
+PathPlanning::PathPlanning() : Node("path_planning")
 {
     this->declare_parameter<std::string>("perception_topic", "/perception");
-
-
+    this->declare_parameter<std::string>("triangulation_topic", "/triangulation"); 
     this->get_parameter("perception_topic", kPerceptionTopic);
+    this->get_parameter("triangulation_topic", kTriangulationTopic);
 
     perception_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-        kPerceptionTopic, 10, std::bind(&PathPlanningNode::perception_callback, this, std::placeholders::_1));
+        kPerceptionTopic, 10, std::bind(&PathPlanning::perception_callback, this, std::placeholders::_1));
+    triangulation_pub_ = this->create_publisher<common_msgs::msg::Triangulation>(kTriangulationTopic, 10);
 }
 
-void PathPlanningNode::perception_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+/**
+ * @brief Create callback function for the perception topic. Main pipeline of the path planning node.
+ * 
+ * @param msg Point Cloud 2 message from the perception topic.
+ */
+void PathPlanning::perception_callback(sensor_msgs::msg::PointCloud2::SharedPtr msg)
 {
-    
+
+
 
 }
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PathPlanningNode>());
+    rclcpp::spin(std::make_shared<PathPlanning>());
     rclcpp::shutdown();
     return 0;
 }
