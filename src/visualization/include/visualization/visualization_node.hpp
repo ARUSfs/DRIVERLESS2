7095cb82bef0c506b4eca5ccd30f8b865c643fae
Ignacio Sánchez <igsais12@gmail.com>
2024-10-20 13:04:12 +0200

    feat: Add triangulation to visualization

--- src/visualization/include/visualization/visualization_node.hpp
+++ src/visualization/include/visualization/visualization_node.hpp
@@ -6,7 +6,9 @@
 #include <rclcpp/rclcpp.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
 #include <visualization_msgs/msg/marker.hpp>
-
+#include <common_msgs/msg/triangulation.hpp>
+#include <common_msgs/msg/simplex.hpp>
+#include <common_msgs/msg/point_xy.hpp>
 
 /**
  * @class Visualization
@@ -22,5 +24,39 @@ class Visualization : public rclcpp::Node
          * @brief Constructor for the Visualization class.
          */
         Visualization();
+    private:
+        /**
+         * @brief Subscriber to the triangulation topic.
+         * Callbacks the triangulation visualization.
+         */
+        rclcpp::Subscription<common_msgs::msg::Triangulation>::SharedPtr triangulation_sub_;
 
+        /**
+         * @brief Publisher to the triangulation visualization topic. 
+         * Publishes the triangulation visualization.
+         */
+        rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr triangulation_visualization_pub_;
+
+        /**
+         * @brief Callback for the triangulation topic.
+         * Receives the triangulation and publishes the visualization.
+         * @param msg common_msgs::msg::Triangulation::SharedPtr Triangulation message.
+         */
+        void triangulation_callback(const common_msgs::msg::Triangulation::SharedPtr msg);
+
+        /**
+         * @brief Topic name for the triangulation topic.
+         */
+        std::string kTriangulationTopic;
+
+        /**
+         * @brief Topic name for the triangulation visualization topic.
+         */
+        std::string kTriangulationVisualizationTopic;
+
+        /**
+         * @brief 
+         * 
+         */
+        rclcpp::Clock::SharedPtr clock_;
 };
--- src/visualization/src/visualization_node.cpp
+++ src/visualization/src/visualization_node.cpp
@@ -15,9 +15,57 @@
  */
 Visualization::Visualization() : Node("visualization")
 {
+    kTriangulationTopic = "/path_planning/triangulation";
+    kTriangulationVisualizationTopic = "/visualization/triangulation";
 
+    triangulation_sub_ = this->create_subscription<common_msgs::msg::Triangulation>(
+        kTriangulationTopic, 10, std::bind(&Visualization::triangulation_callback, this, std::placeholders::_1));
+
+    triangulation_visualization_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>(
+        kTriangulationVisualizationTopic, 10);
 }
 
+void Visualization::triangulation_callback(const common_msgs::msg::Triangulation::SharedPtr msg)
+{
+    visualization_msgs::msg::MarkerArray marker_array_;
+    for (int i = 0; i < msg -> simplices.size(); i++){
+        visualization_msgs::msg::Marker marker_;
+        marker_.header.frame_id = "map";
+        marker_.header.stamp = clock_->now();
+        marker_.ns = "visualization";
+        marker_.id = i;
+        marker_.type = visualization_msgs::msg::Marker::LINE_STRIP;
+        marker_.action = visualization_msgs::msg::Marker::ADD;
+        marker_.pose.position.x = 1.0;
+        marker_.pose.position.y = 0;
+        marker_.pose.position.z = 0;
+        marker_.scale.x = 1.0;
+        marker_.scale.y = 1.0;
+        marker_.scale.z = 1.0;
+        marker_.color.a = 1.0;
+        marker_.color.r = 1.0;
+        marker_.color.g = 0.0;
+        marker_.color.b = 0.0;
+        marker_.lifetime = rclcpp::Duration::from_seconds(0.1);
+        for (int j=0; j<3; j++){
+            geometry_msgs::msg::Point p;
+            p.x = msg->simplices[i].points[j].x;
+            p.y = msg->simplices[i].points[j].y;
+            p.z = 0;
+            marker_.points.push_back(p);
+        }
+        geometry_msgs::msg::Point p;
+        p.x = msg->simplices[i].points[0].x;
+        p.y = msg->simplices[i].points[0].y;
+        p.z = 0;
+        marker_.points.push_back(p);
+
+        marker_array_.markers.push_back(marker_);
+
+    }
+    
+    
+}
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
