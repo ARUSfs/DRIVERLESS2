329946d11597804b713f02b3bfa2e8d4c18a06bb
alvarolp02 <alplepe02@gmail.com>
2024-10-26 03:59:10 +0200

    feat: configuration added to arussim interface

--- src/arussim_interface/include/arussim_interface/arussim_interface_node.hpp
+++ src/arussim_interface/include/arussim_interface/arussim_interface_node.hpp
@@ -28,22 +28,55 @@ class ARUSSimInterface : public rclcpp::Node
          * @brief Constructor for the ARUSSimInterface class.
          */
         ARUSSimInterface();
-
+    
+    private:
+        //Callbacks
         void cmd_callback(const common_msgs::msg::Cmd::SharedPtr msg);
         void cmd4wd_callback(const common_msgs::msg::Cmd4WD::SharedPtr msg);
         void wheel_speeds_callback(const arussim_msgs::msg::FourWheelDrive::SharedPtr msg);
         void trajectory_callback(const arussim_msgs::msg::Trajectory::SharedPtr msg);
-    
-    private:
 
+        // Publishers
         rclcpp::Publisher<arussim_msgs::msg::Cmd>::SharedPtr cmd_pub_;
         rclcpp::Publisher<arussim_msgs::msg::Cmd4WD>::SharedPtr cmd4wd_pub_;
         rclcpp::Publisher<common_msgs::msg::FourWheelDrive>::SharedPtr wheel_speeds_pub_;
         rclcpp::Publisher<common_msgs::msg::Trajectory>::SharedPtr trajectory_pub_;
 
+        // Subscribers
         rclcpp::Subscription<common_msgs::msg::Cmd>::SharedPtr cmd_sub_;
         rclcpp::Subscription<common_msgs::msg::Cmd4WD>::SharedPtr cmd4wd_sub_;
         rclcpp::Subscription<arussim_msgs::msg::FourWheelDrive>::SharedPtr wheel_speeds_sub_;
         rclcpp::Subscription<arussim_msgs::msg::Trajectory>::SharedPtr trajectory_sub_;
 
+        // Topics names
+        std::string kSimCmdTopic;
+        std::string kSimCmd4WDTopic;
+        std::string kSimWheelSpeedsTopic;
+        std::string kSimTrajectoryTopic;
+        std::string kPipelineCmdTopic;
+        std::string kPipelineCmd4WDTopic;
+        std::string kPipelineWheelSpeedsTopic;
+        std::string kPipelineTrajectoryTopic;
+
+
+         // Load parameters from the config file
+        void get_config_parameters(){
+            this->declare_parameter("sim_cmd_topic", "/arussim/cmd");
+            this->declare_parameter("sim_cmd4wd_topic", "/arussim/cmd4wd");
+            this->declare_parameter("sim_wheel_speeds_topic", "/arussim/wheel_speeds");
+            this->declare_parameter("sim_trajectory_topic", "/arussim/fixed_trajectory");
+            this->declare_parameter("pipeline_cmd_topic", "/controller/cmd");
+            this->declare_parameter("pipeline_cmd4wd_topic", "/controller/cmd4wd");
+            this->declare_parameter("pipeline_wheel_speeds_topic", "/arussim_interface/wheel_speeds");
+            this->declare_parameter("pipeline_trajectory_topic", "/arussim_interface/fixed_trajectory");
+
+            this->get_parameter("sim_cmd_topic", kSimCmdTopic);
+            this->get_parameter("sim_cmd4wd_topic", kSimCmd4WDTopic);
+            this->get_parameter("sim_wheel_speeds_topic", kSimWheelSpeedsTopic);
+            this->get_parameter("sim_trajectory_topic", kSimTrajectoryTopic);
+            this->get_parameter("pipeline_cmd_topic", kPipelineCmdTopic);
+            this->get_parameter("pipeline_cmd4wd_topic", kPipelineCmd4WDTopic);
+            this->get_parameter("pipeline_wheel_speeds_topic", kPipelineWheelSpeedsTopic);
+            this->get_parameter("pipeline_trajectory_topic", kPipelineTrajectoryTopic);
+        }
 };
