d0261d66e5b07f3f01b5bbbc579f8b4ac74ff547
alvarolp02 <alplepe02@gmail.com>
2024-10-26 03:07:54 +0200

    feat: simulation launch created

---
+++ src/common/common_meta/launch/simulation_launch.py
@@ -0,0 +1,22 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    planning_launch_path = os.path.join(get_package_share_directory('path_planning'), 
+                                        'launch', 'path_planning_launch.py')
+    visualization_launch_path = os.path.join(get_package_share_directory('visualization'), 
+                                        'launch', 'visualization_launch.py')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(planning_launch_path)),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(visualization_launch_path)),
+    ])
