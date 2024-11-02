b56a394a49454fa517f974f6c4a06bf4403aeafa
Ignacio Sánchez <igsais12@gmail.com>
2024-11-02 16:54:42 +0100

    feat: create file containing simplex_tree class and methods to implement the path planning algorithm

---
+++ src/path_planning/include/path_planning/simplex_tree.hpp
@@ -0,0 +1,46 @@
+/**
+ * @file simplex_tree.hpp
+ * @author Ignacio Sánchez Isidro (igsais12@gmail.com)
+ * @date 01-11-2024
+ * @brief Creates a tree structure to store information about neighbor triangles.
+ * It also contain helper functions such as a search function.
+ * 
+ */
+
+/**
+ * @brief Class for the simlpex tree structure.
+ * Nodes are indices of the triangles in the triangulation. Two nodes are connected if they share an edge.
+ */
+class simplex_tree
+{
+public:
+    /**
+     * @brief Index of the triangle in the triangulation. int type.
+     */
+    int index;
+
+    /**
+     * @brief Right and left children of the node. Recursive structure for tree construction.
+     */
+    simplex_tree *right, *left;
+
+    /**
+     * @brief Construct a new simplex tree object.
+     * @param key int index of the triangle.
+     */
+    simplex_tree(int key);
+};
+
+simplex_tree::simplex_tree(int key)
+{
+    index = key;
+    right = nullptr;
+    left = nullptr;
+}
+
+bool tree_search (int key, simplex_tree *root){
+    if (root == nullptr) return false;
+    if (root->index == key) return true;
+    return tree_search(key, root->left) || tree_search(key, root->right);
+}
+
