bd31cd5d96fddf31e5187176f9a010c4468a3a8d
David Guil <davidguilb1@gmail.com>
2024-11-05 18:46:37 +0100

    feat: add clase

---
+++ src/planning/acc_planning/include/acc_planning/acc_planning_node.hpp
@@ -0,0 +1,24 @@
+/**
+ * 
+ * 
+ * @file acc_planning_node.hpp
+ * @author David Guil Barranco (davidguilb1@gmail.com)
+ * @brief Header file for the Acc Planning node. 
+ * Contains the class definition and the declaration of the methods 
+ * used in the algorithm.
+ * @version 0.1
+ * @date 5-11-2024
+ */
+#include <rclcpp/rclcpp.hpp>
+
+class AccPlanning: public rclcpp::Node{
+public:
+   
+    AccPlanning() {
+        
+        
+    };
+private:
+
+};
+
