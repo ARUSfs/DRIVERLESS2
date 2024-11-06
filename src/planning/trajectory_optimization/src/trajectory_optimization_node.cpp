344d2acc0506a62f86ba8458fd5682450b3d7fa4
--global <--global>
2024-11-06 18:24:44 +0100

    fix: change pub, sub names and cmakelist

--- src/planning/trajectory_optimization/src/trajectory_optimization_node.cpp
+++ src/planning/trajectory_optimization/src/trajectory_optimization_node.cpp
@@ -2,14 +2,14 @@
 
 TrajectoryOptimization::TrajectoryOptimization() : Node("trajectory_optimization")
 {
-    arussim_sub_ = this->create_subscription<common_msgs::msg::Trajectory>(
-        "/arussim_interface/fixed_trajectory", 10, std::bind(&TrajectoryOptimization::arussim_callback, this, std::placeholders::_1));
+    trajectory_sub_ = this->create_subscription<common_msgs::msg::Trajectory>(
+        "/arussim_interface/fixed_trajectory", 10, std::bind(&TrajectoryOptimization::trajectory_callback, this, std::placeholders::_1));
     car_state_sub_ = this->create_subscription<common_msgs::msg::State>(
         "/car_state/state", 1, std::bind(&TrajectoryOptimization::car_state_callback, this, std::placeholders::_1));
-    trajectory_pub_ = this->create_publisher<common_msgs::msg::Trajectory>("/trajectory_optimizer/trajectory", 10);
+    optimized_trajectory_pub_ = this->create_publisher<common_msgs::msg::Trajectory>("/trajectory_optimizer/trajectory", 10);
 }
 
-void TrajectoryOptimization::arussim_callback(common_msgs::msg::Trajectory::SharedPtr assim_msg){
+void TrajectoryOptimization::trajectory_callback(common_msgs::msg::Trajectory::SharedPtr trajectory_msg){
 
 }
 
