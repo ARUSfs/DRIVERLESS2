848d2e9966c84b61c8889b29a4f7a568d293eb96
Alejandro <alejandro.vm.1805@gmail.com>
2024-11-09 21:00:51 +0100

    feat: create lidar subscriber

--- src/perception/src/perception_node.cpp
+++ src/perception/src/perception_node.cpp
@@ -2,6 +2,8 @@
  * @file perception_node.hpp
  * @author Alejandro Vallejo Mayo (alejandro.vm.1805@gmail.com)
  * @brief Main file for the Perception node. 
+ * Contains the main function and the implementation of the methods to achieve a robust and reliable perception algorithm for the ARUS Team 
+ * which extracts the location of the cones on the track.
  * @version 0.1
  * @date 3-11-2024
  */
@@ -10,7 +12,22 @@
 
 Perception::Perception() : Node("Perception")
 {
+    //Get the parameters
+    this->declare_parameter<std::string>("lidar_topic", "/rslidar_points");
+    this->get_parameter("lidar_topic", kLidarTopic);
     
+    //Create the subscriber
+    lidar_sub_ = this -> create_subscription<sensor_msgs::msg::PointCloud2>(
+        kLidarTopic, 10, std::bind(&Perception::lidar_callback, this, std::placeholders::_1));
+}
+
+/**
+ * @brief Create callback function for the lidar topic.
+ * @param lidar_msg The point cloud message received from the lidar topic.
+ */
+void Perception::lidar_callback(const sensor_msgs::msg::PointCloud2::SharedPtr lidar_msg)
+{   
+
 }
 
 int main(int argc, char * argv[])
