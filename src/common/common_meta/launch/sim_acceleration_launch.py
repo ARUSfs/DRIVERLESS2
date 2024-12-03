c3c4260b2734b4631149ab68d6af7649a0e64940
alvarolp02 <alplepe02@gmail.com>
2024-12-03 18:43:40 +0100

    feat: add launch files

---
+++ src/common/common_meta/launch/sim_acceleration_launch.py
@@ -0,0 +1,39 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        create_node(pkg='acc_planning'),
+        create_node(pkg='controller',
+                    params=[{'trajectory': "/acc_planning/trajectory",
+                             'target': 10.0}]),
+        create_node(pkg='visualization'),
+        create_node(pkg='arussim_interface'),
+        create_node(pkg='car_state', 
+                    params=[{'simulation': True}]),
+        create_node(pkg='icp_slam')
+    ])
+
+
+def create_node(pkg, config=None, exec=None, params=[]): 
+
+    if config is None:
+        config = pkg + "_config.yaml"
+    if exec is None:
+        exec = pkg + "_exec"
+
+    package_share_directory = get_package_share_directory(pkg)
+    config_file = os.path.join(package_share_directory, "config", config)
+
+    return Node(
+        package=pkg,
+        executable=exec,
+        name=pkg,
+        output="screen",
+        parameters=[config_file]+params
+        )
