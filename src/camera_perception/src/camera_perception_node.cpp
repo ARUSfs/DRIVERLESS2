a9f5c6b963a7563be8ab7f0338cf946c8fc2e238
Ignacio Sánchez Isidro <148434224+igsais@users.noreply.github.com>
2025-03-08 20:21:54 +0100

    feat: add parameter to decide to show the image

--- src/camera_perception/src/camera_perception_node.cpp
+++ src/camera_perception/src/camera_perception_node.cpp
@@ -14,6 +14,8 @@ CameraPerception::CameraPerception() : Node("camera_perception")
     this->get_parameter("image_topic", kImageTopic);
     this->declare_parameter<int>("camera_period", 100);
     this->get_parameter("camera_period", kCameraPeriod);
+    this->declare_parameter<bool>("show_image", false);
+    this->get_parameter("show_image", kShowImage);
 
     image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("image", 10);
     camera_timer_ = this->create_wall_timer(std::chrono::milliseconds(kCameraPeriod),
@@ -34,7 +36,11 @@ void CameraPerception::camera_callback(){
     }
     sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frame).toImageMsg();
     image_pub_->publish(*msg);
-
+    if (kShowImage){
+        imshow("Camera", frame);
+        cv::waitKey();
+    }
+        
 }
 
 
