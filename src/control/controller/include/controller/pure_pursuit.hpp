32761c5b6154f184942ffc91c6d1295909604dee
lolahzc <lolahercan@gmail.com>
2025-03-15 19:28:55 +0100

    fix: new parameters

--- src/control/controller/include/controller/pure_pursuit.hpp
+++ src/control/controller/include/controller/pure_pursuit.hpp
@@ -128,7 +128,7 @@ public:
      double calculate_look_ahead_distance(double cross_track_error) {
         double delta_min_ = 1.0;  
         double delta_max_ = 8.0; 
-        double gamma_ = 0.8;      
+        double gamma_ = 0.8;        
 
         return (delta_max_ - delta_min_) * std::exp(-gamma_ * std::abs(cross_track_error)) + delta_min_;
     }
