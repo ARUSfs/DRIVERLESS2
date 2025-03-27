a6b20c5dca062c9196746219b21705827d28d724
Ignacio Sánchez <igsais12@gmail.com>
2025-03-27 20:44:04 +0100

    feat: include darknet and detect in camera callback

--- src/camera_perception/include/camera_perception/camera_perception_node.hpp
+++ src/camera_perception/include/camera_perception/camera_perception_node.hpp
@@ -7,11 +7,13 @@
  */
 
 #include <rclcpp/rclcpp.hpp>
+#include <ament_index_cpp/get_package_share_directory.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <std_msgs/msg/header.hpp>
 
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <DarkHelp.hpp>
 
 
 
@@ -20,16 +22,25 @@ class CameraPerception : public rclcpp::Node
     public:
         CameraPerception();
     private:
+        // Config file
         std::string kImageTopic;
         int kCameraPeriod;
         int kCameraID;
-        //rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+        
+        // Darknet params
+        std::string kConfigFile;
+        std::string kWeightsFile;
+        std::string kNamesFile;
+
+        DarkHelp::NN nn_;
+
+        rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
         rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub_;
         rclcpp::TimerBase::SharedPtr camera_timer_;
-
+        
         cv::VideoCapture camera;
-
+        
         void camera_callback();
-
+        void image_callback(sensor_msgs::msg::Image::SharedPtr msg);
 
     };
--- src/camera_perception/src/camera_perception_node.cpp
+++ src/camera_perception/src/camera_perception_node.cpp
@@ -17,18 +17,35 @@ CameraPerception::CameraPerception() : Node("camera_perception")
     this->declare_parameter<int>("camera_id", 0);
     this->get_parameter("camera_id", kCameraID);
 
+    // Darknet params
+    std::string pkg_path = ament_index_cpp::get_package_share_directory("camera_perception");
+    kConfigFile = pkg_path + "/resources/darknet/cfg.cfg";
+    kWeightsFile = pkg_path + "/resources/darknet/weights";
+    kNamesFile = pkg_path + "/resources/darknet/names";
+
+    std::cout << kImageTopic << std::endl;
+    
+    rclcpp::QoS qos(20);
+    qos.best_effort();
+    image_sub_ = this->create_subscription<sensor_msgs::msg::Image>(kImageTopic, qos,
+                    std::bind(&CameraPerception::image_callback, this, std::placeholders::_1));
+
     image_pub_ = this->create_publisher<sensor_msgs::msg::Image>("image", 20);
-    camera_timer_ = this->create_wall_timer(std::chrono::milliseconds(kCameraPeriod),
-                    std::bind(&CameraPerception::camera_callback, this));
-    camera.open(kCameraID, cv::CAP_V4L2);
-    camera.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-    camera.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-    camera.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
-    camera.set(cv::CAP_PROP_FPS, 15);
-    std::cout << camera.get(cv::CAP_PROP_ZOOM) << std::endl;
+    // camera_timer_ = this->create_wall_timer(std::chrono::milliseconds(kCameraPeriod),
+    //                 std::bind(&CameraPerception::camera_callback, this)); 
+    
+    // Darknet
+    nn_.init(kConfigFile, kWeightsFile, kNamesFile);
+    nn_.config.threshold = 0.1;
+    nn_.config.include_all_names = true;
+    nn_.config.names_include_percentage = true;
+    nn_.config.annotation_include_duration = false;
+    nn_.config.annotation_include_timestamp = false;
+    nn_.config.sort_predictions = DarkHelp::ESort::kAscending;
 }
 
 void CameraPerception::camera_callback(){
+    return;
     if (!camera.isOpened()){
         RCLCPP_ERROR(this->get_logger(), "Camera not opened");
         return;
@@ -43,8 +60,20 @@ void CameraPerception::camera_callback(){
     cv::resize(frame, frame_resized, cv::Size(640, 480));
     sensor_msgs::msg::Image::SharedPtr msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", frame_resized).toImageMsg();
     image_pub_->publish(*msg);
+}
+
+void CameraPerception::image_callback(const sensor_msgs::msg::Image::SharedPtr msg){
+    cv::Mat image = cv_bridge::toCvCopy(msg, "bgr8")->image;
+    cv::Mat image_resized;
+    cv::resize(image, image_resized, cv::Size(1920, 1088));
+    const auto result = nn_.predict(image_resized);
     
-        
+    // Results visualization
+    std::cout << result << std::endl;
+    cv::Mat output = nn_.annotate();
+    sensor_msgs::msg::Image::SharedPtr msg_out = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", output).toImageMsg();
+    image_pub_->publish(*msg_out);
+
 }
 
 
