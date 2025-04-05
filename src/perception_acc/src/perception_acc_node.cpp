6dcd9f7abf126910d74b48ddf8a6c0bfb5de7e7d
Alvaro Galisteo <galisbermo03@gmail.com>
2025-04-05 14:08:50 +0200

    fix: fix the name the of the package

---
+++ src/perception_acc/src/perception_acc_node.cpp
@@ -0,0 +1,252 @@
+/**
+ * @file perception_acc_node.hpp
+ * @author Álvaro Galisteo Bermúdez (galisbermo03@gmail.com)
+ * @brief Main file for the Perception Accumulation node. 
+ * Contains the main function and the implementation of the methods to achieve a robust and reliable perception algorithm for the ARUS 
+ * Team, which extracts the location of the cones on the track.
+ * @version 0.1
+ * @date 11-3-2025
+ */
+
+#include "perception_acc/perception_acc_node.hpp"
+bool DEBUG = false;
+
+Perception::Perception() : Node("Perception")
+{
+    //Declare the parameters
+    this->declare_parameter<std::string>("lidar_topic", "/rslidar_points");
+    this->declare_parameter<std::string>("state_topic", "/car_state/state");
+    this->declare_parameter<bool>("crop", true);
+    this->declare_parameter<double>("max_x_fov", 25.0);
+    this->declare_parameter<double>("max_y_fov", 20.0);
+    this->declare_parameter<double>("max_z_fov", 0.0);
+    this->declare_parameter<double>("threshold_ground_filter", 0.05);
+    this->declare_parameter<double>("radius", 1.0);
+    this->declare_parameter<int>("number_sections", 8);
+    this->declare_parameter<double>("angle_threshold", 20);
+    this->declare_parameter<int>("minimum_ransac_points", 30);
+    this->declare_parameter<double>("threshold_scoring", 0.7);
+    this->declare_parameter<double>("distance_threshold", 0.4);
+    this->declare_parameter<double>("coloring_threshold", 0.4);
+
+    //Get the parameters
+    this->get_parameter("lidar_topic", kLidarTopic);
+    this->get_parameter("state_topic", kStateTopic);
+    this->get_parameter("crop", kCrop);
+    this->get_parameter("max_x_fov", kMaxXFov);
+    this->get_parameter("max_y_fov", kMaxYFov);
+    this->get_parameter("max_z_fov", kMaxZFov);
+    this->get_parameter("threshold_ground_filter", kThresholdGroundFilter);
+    this->get_parameter("radius", kRadius);
+    this->get_parameter("number_sections", kNumberSections);
+    this->get_parameter("angle_threshold", kAngleThreshold);
+    this->get_parameter("minimum_ransac_points", kMinimumRansacPoints);
+    this->get_parameter("threshold_scoring", kThresholdScoring);
+    this->get_parameter("distance_threshold", kDistanceThreshold);
+    this->get_parameter("coloring_threshold", kColoringThreshold);
+
+    // Initialize the variables
+    vx = 0.0;
+    vy = 0.0;
+    yaw = 0.0;
+    dt = 0.1;
+
+    //Transform into radians
+    kHFov *= (M_PI/180);
+    
+    //Create the subscribers
+    lidar_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+        kLidarTopic, 10, std::bind(&Perception::lidar_callback, this, std::placeholders::_1));
+
+    state_sub_ = this->create_subscription<common_msgs::msg::State>(
+            kStateTopic, 10, std::bind(&Perception::state_callback, this, std::placeholders::_1));
+    
+    //Create the publishers
+    filtered_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+        "/perception/filtered_cloud", 10);
+    clusters_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+        "/perception/clusters", 10);
+    map_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
+        "/perception/map", 10);
+}
+
+/**
+ * @brief Extract the center of each cluster.
+ * @param cluster_indices The indices of the points that form each cluster.
+ * @param cloud_filtered The input point cloud.
+ * @param cluster_centers The center of each cluster.
+ */
+void Perception::get_clusters_centers(std::vector<pcl::PointIndices>& cluster_indices,
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered, std::vector<PointXYZColorScore>& clusters_centers)
+{
+    for (auto it = cluster_indices.begin(); it != cluster_indices.end(); )
+    {
+        //Create a temporal point cloud
+        pcl::PointCloud<pcl::PointXYZI>::Ptr cluster_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::copyPointCloud(*cloud_filtered, *it, *cluster_cloud);
+
+        //Obtain the new bounding box of the cluster
+        pcl::PointXYZI min_point, max_point;
+        pcl::getMinMax3D(*cluster_cloud, min_point, max_point);
+        double max_x = max_point.x;
+        double min_x = min_point.x;
+        double max_y = max_point.y;
+        double min_y = min_point.y;
+        double max_z = max_point.z;
+        double min_z = min_point.z;
+
+        //Filter the cluster by size and keep the center of the cluster
+        if ((max_z-min_z)<0.4 && min_z<0.0 && (max_x-min_x)<0.5 && (max_y-min_y)<0.5)
+        {
+            Eigen::Vector4f centroid;
+            pcl::compute3DCentroid(*cluster_cloud, centroid);
+            PointXYZColorScore center;
+            center.x = centroid[0];
+            center.y = centroid[1];
+            center.z = min_z;
+            center.color = 0;
+            center.score = 0;
+            clusters_centers.push_back(center);
+
+            it++;
+        }
+        else
+        {
+            it = cluster_indices.erase(it);
+        }
+    }
+    //Resize the cluster indices vector
+    cluster_indices.resize(clusters_centers.size());
+}
+
+
+/**
+ * @brief Recover falsely ground filtered points.
+ * @param cloud_plane The input point cloud.
+ * @param cloud_filtered The filtered point cloud.
+ * @param cluster_indices The indices of the points that form each cluster.
+ * @param cluster_centers The center of each cluster.
+ * @param radius The radius used to search for eliminated points.
+ */
+void Perception::reconstruction(pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_plane, pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered, 
+    std::vector<pcl::PointIndices>& cluster_indices, std::vector<PointXYZColorScore> clusters_centers, 
+    double radius)
+{
+    //Iterate on clusters
+    for (size_t i = 0; i < clusters_centers.size(); ++i)
+    {
+        //Convert from PointXYZColorScore to PointXYZI
+        pcl::PointXYZI center;
+        center.x = clusters_centers[i].x;
+        center.y = clusters_centers[i].y;
+        center.z = clusters_centers[i].z;
+        center.intensity = clusters_centers[i].score;
+        
+        //Iterate on planar points
+        for (size_t j = 0; j < cloud_plane->size(); ++j)
+        {
+            pcl::PointXYZI point = cloud_plane->points[j];
+
+            //Check if the point lies inside the cylinder
+            double dx = point.x - center.x;
+            double dy = point.y - center.y;
+            double radial_distance = std::sqrt(dx * dx + dy * dy);
+
+            //Insert the recovered points
+            if (radial_distance <= radius)
+            {
+                //Add the point to the filtered cloud
+                cloud_filtered->points.push_back(point);
+                cloud_filtered->width++;
+
+                //Add the index of the recovered point to cluster indices
+                cluster_indices[i].indices.push_back(cloud_filtered->points.size() - 1);
+            }
+        }
+    }
+}
+
+/**
+ * @brief Filter the final clusters by size to delete the ones that are too small or too large to be considered cones.
+ * @param cluster_indices The indices of the points that form each cluster.
+ * @param cloud_filtered The input point cloud.
+ * @param cluster_centers The center of each cluster.
+ */
+void Perception::filter_clusters(std::vector<pcl::PointIndices>& cluster_indices,
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered, std::vector<PointXYZColorScore>& clusters_centers)
+{
+    for (int i = cluster_indices.size() - 1; i >= 0; i--)
+    {
+        //Create a temporal point cloud
+        pcl::PointIndices indices = cluster_indices[i];
+        pcl::PointCloud<pcl::PointXYZI>::Ptr cluster_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+        pcl::copyPointCloud(*cloud_filtered, indices, *cluster_cloud);
+
+        //Obtain the new bounding box of the cluster
+        pcl::PointXYZI min_point, max_point;
+        pcl::getMinMax3D(*cluster_cloud, min_point, max_point);
+        double max_x = max_point.x;
+        double min_x = min_point.x;
+        double max_y = max_point.y;
+        double min_y = min_point.y;
+        double max_z = max_point.z;
+        double min_z = min_point.z;
+
+        //Filter the cluster by size and delete the ones that are too small or too large
+        if ((max_z - min_z) < 0.10 || (max_z - min_z) > 0.4 || (max_x - min_x) > 0.5 || (max_y - min_y) > 0.5)
+        {
+            clusters_centers.erase(clusters_centers.begin() + i);
+            cluster_indices.erase(cluster_indices.begin() + i);
+        }
+    }
+    
+    //Resize the vectors
+    clusters_centers.resize(clusters_centers.size());
+    cluster_indices.resize(clusters_centers.size());
+}
+
+
+/**
+* @brief Create callback function for the car state topic.
+* @param state_msg The information received from the car state node.
+*/
+void Perception::state_callback(const common_msgs::msg::State::SharedPtr state_msg)
+{
+    vx = state_msg->vx;
+    vy = state_msg->vy;
+    x = state_msg->x;
+    y = state_msg->y;
+    yaw = state_msg->yaw;
+}
+
+/**
+ * @brief Create callback function for the lidar topic.
+ * @param lidar_msg The point cloud message received from the lidar topic.
+ */
+void Perception::lidar_callback(const sensor_msgs::msg::PointCloud2::SharedPtr lidar_msg)
+{   
+    //Define the variables for the ground filter
+    pcl::PointCloud<PointXYZIRingTime>::Ptr cloud(new pcl::PointCloud<PointXYZIRingTime>);
+    pcl::fromROSMsg(*lidar_msg, *cloud);
+
+    bool global_acc = false;
+    pcl::PointCloud<PointXYZIRingTime>::Ptr updated_cloud;
+    if (global_acc) updated_cloud = Accumulation::accumulate_global_cloud_ring(cloud, x, y, yaw, dt);
+    else updated_cloud = Accumulation::accumulate_local_cloud_ring(cloud, x, y, yaw, dt);
+
+    RCLCPP_INFO(this->get_logger(), "Accumulated cloud size: %zu", updated_cloud->size());
+
+
+    sensor_msgs::msg::PointCloud2 filtered_msg;
+    pcl::toROSMsg(*updated_cloud, filtered_msg);
+    filtered_msg.header.frame_id="/map";
+    filtered_pub_->publish(filtered_msg);
+}
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Perception>());
+    rclcpp::shutdown();
+    return 0;
+}
